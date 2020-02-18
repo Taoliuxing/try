dfe8c322fc160a80a01af622c3b7bb2e1ead81af
Taoliuxing <1095295368@qq.com>
2020-02-18 23:16:00 +0800

    test

---
+++ testwebpack/src/index.js
@@ -0,0 +1,5 @@
+import img from './img/10.jpg';
+console.log('asdasdasd');
+var oImg = new Image();
+oImg.src = img;
+document.body.append(oImg);
---
+++ webpack/src/index.js
@@ -0,0 +1,5 @@
+import img from './img/10.jpg';
+console.log('第一次webpack');
+var oImg = new Image();
+oImg.src = img;
+document.body.append(oImg);
