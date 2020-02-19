0c307cc77ce2e8c98dc56ad2e573c3a19e64153f
Taoliuxing <1095295368@qq.com>
2020-02-19 10:24:12 +0800

    打包css

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,5 @@
 import img from './img/10.jpg';
+import style from './css/index.css';
 console.log('第一次webpack');
 var oImg = new Image();
 oImg.src = img;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,27 +1,35 @@
 // node引入模块的方式
 const path = require("path");
 module.exports = {
+    mode: "development",
     // 入口文件 
-    entry:"./src/index.js",
+    entry: "./src/index.js",
     // 出口文件 打包到哪
-    output:{
-        filename:"bundle.js",
+    output: {
+        filename: "bundle.js",
         // path是绝对路径 
-        path:path.resolve(__dirname,"dist")
+        path: path.resolve(__dirname, "dist")
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.(jpg|png|gif)$/,
-                use:{
-                    loader:'url-loader',
-                    options:{
-                        outputPath:'images/',
-                        limit:100,
-                        name:'[name].[ext]'
+                test: /\.(jpg|png|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        outputPath: 'images/',
+                        limit: 100,
+                        name: '[name].[ext]'
                     }
                 }
-        }
-    ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
     }
 }
