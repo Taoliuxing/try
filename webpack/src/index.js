3e518b265bb7f6568a2e6e74b3c9ad2d7bfefdc7
Taoliuxing <1095295368@qq.com>
2020-02-19 12:21:16 +0800

    c3

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,6 @@
 import img from './img/10.jpg';
-import style from './css/index.css';
+// import style from './css/index.css';
+import style from './css/index2.scss';
 console.log('第一次webpack');
 var oImg = new Image();
 oImg.src = img;
