9155f070f1c9f583752b7ecaffeb823028506011
Taoliuxing <1095295368@qq.com>
2020-02-25 20:25:15 +0800

    vuejsj

---
+++ project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <first-page>
+      <common></common>
+    </first-page>
+    <not-common></not-common>
+  </div>
+</template>
+
+<script>
+  import Common from './components/Common';
+  import NotCommon from './components/NotCommon';
+  import FirstPage from './views/FirstPage';
+  export default {
+      components:{
+        Common,
+        NotCommon,
+        FirstPage
+      }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ project/src/views/FirstPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <slot></slot>
+        <hr>
+        <h2>this is my first vue page</h2>
+        <p>thank you</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
