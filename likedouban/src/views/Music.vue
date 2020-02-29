d9c4fc78a545384d74cf2ade655232f8b6476822
Taoliuxing <1095295368@qq.com>
2020-02-29 15:34:14 +0800

    music one

--- likedouban/src/views/Music.vue
+++ likedouban/src/views/Music.vue
@@ -1,15 +1,31 @@
 <template>
     <div>
-        music
+        <img v-for="music in musicList" :key="music.id" :src="music.bg" alt="">
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
-        
+        data(){
+            return {
+                musicList:[]
+            }
+        },
+        created(){
+            axios.get('data/musiclist.json')
+            .then((res)=>{
+                this.musicList = res.data.albums;
+            }).catch((res)=>{
+
+            })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    img{
+        width:50%;
+        float: left;    
+    }
 </style>
