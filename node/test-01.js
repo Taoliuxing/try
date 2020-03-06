8a3907115ebd55682cf45519fb2f99985d1d4cf7
Taoliuxing <1095295368@qq.com>
2020-03-06 11:15:35 +0800

    xjbg

---
+++ node/test-01.js
@@ -0,0 +1,17 @@
+//     1
+// for(var i=0;i<5;i++){
+//     setTimeout(function(){
+//         console.log(i)
+//     },0)
+// }
+// console.log("end")
+
+
+//    2
+setTimeout(function(){
+    console.log(123)
+},1);
+
+setTimeout(function(){
+    console.log(567)
+},0);
