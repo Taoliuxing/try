1b185c0f82936d571803d55dc2d737670b813c03
Taoliuxing <1095295368@qq.com>
2020-04-22 09:00:13 +0800

    ?

---
+++ vuetest/src/components/CommonFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <router-link 
+        v-for="(obj,index) in menu" 
+        :key="index" 
+        :to="obj.path"
+        >
+        {{obj.title}}
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                menu:[
+                        {
+                            title:'电影',
+                            path:'/movie'
+                        }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
